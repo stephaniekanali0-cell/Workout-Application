21ecb661e233ea17b7d0f41ee50055c23b392471
Stephaneie Kanali <stephaniekanali0@gmail.com>
2026-07-27 20:05:43 +0300

    Added workouts table and updated exercise model in models.py

--- server/models.py
+++ server/models.py
@@ -8,3 +8,10 @@ class Exercise(db.Model):
     name = db.Column(db.String(80), nullable=False, unique= True)
     category = db.Column(db.String, nullable= False)
     equipment_needed = db.Column(db.Boolean, nullable=False)
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.String(100), nullable=False)
